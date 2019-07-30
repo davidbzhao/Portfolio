e16b1245fd58af27a020324d2d69497e181be14f
David Zhao <dz6hu@virginia.edu>
2019-07-30 07:12:50 +0000

    Create model for event

--- intro/models.py
+++ intro/models.py
@@ -1,3 +1,16 @@
 from django.db import models
 
-# Create your models here.
+
+class Event(models.Model):
+    EVENT_TYPES = (
+        ('E', 'Education'),
+        ('L', 'Location'),
+        ('W', 'Work')
+    )
+    event_type = models.CharField(max_length=1, choices=EVENT_TYPES)
+    title = models.CharField(max_length=128)
+    title_prefix = models.CharField(max_length=16)
+    subtitle = models.CharField(max_length=128, blank=True)
+    description = models.TextField(blank=True)
+    start_date = models.DateField()
+    end_date = models.DateField(blank=True, null=True)
--- intro/views.py
+++ intro/views.py
@@ -1,5 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+from intro.models import Event
+
 def index(request):
-    return HttpResponse('Hello World!')
+    context = {
+        'events': Event.objects.all()
+    }
+    return render(request, 'intro/index.html', context)
