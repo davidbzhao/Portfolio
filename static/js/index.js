97b679d15a539a3278e3ee29a02092d04763ed3f
David Zhao <dz6hu@virginia.edu>
2019-08-01 04:22:27 +0000

    Change VH to custom JS

--- static/js/index.js
+++ static/js/index.js
@@ -1,4 +1,57 @@
+const NAME_HEIGHT_VH = 100;
+const STICKY_TOP_VH = 40;
+const EVENT_MARGIN_BOTTOM_VH = 50;
+const HEIGHT_CHANGE_THRESHOLD = 100;
+
+let prevInnerHeight = window.innerHeight;
+let vh = prevInnerHeight * 0.01;
+
+const updatePrefix = () => {
+    const prefix = document.getElementsByClassName('prefix')[0];
+    const prefixText = prefix.getElementsByTagName('h1')[0];
+
+    prefixText.style.top = `${STICKY_TOP_VH * vh}px`;
+}
+
+const updateNameCard = () => {
+    const nameCard = document.getElementById('name');
+    nameCard.style.height = `${NAME_HEIGHT_VH * vh}px`;
+    
+    const nameText = nameCard.getElementsByTagName('h1')[0];
+    nameText.style.top = `${STICKY_TOP_VH * vh}px`;
+}
+
+const updateEventCards = () => {
+    const eventDivs = document.getElementsByClassName('event');
+    Array.from(eventDivs).map(el => {
+        el.style.marginBottom = `${EVENT_MARGIN_BOTTOM_VH * vh}px`;
+    });
+}
+
+const updateContactCard = () => {
+    const contactCard = document.getElementById('contact');
+    const newMarginBottom = window.innerHeight - window.innerHeight * 0.01 * 40 - contactCard.offsetHeight;
+    contactCard.style.marginBottom = `${newMarginBottom}px`;
+    console.log(window.innerHeight);
+    console.log(STICKY_TOP_VH * window.innerHeight * 0.01);
+    console.log(contactCard.offsetHeight);
+    console.log(contactCard.clientHeight);
+    console.log(contactCard.scrollHeight);
+    console.log(window.innerHeight - window.innerHeight * 0.01 * 40 - contactCard.offsetHeight)
+}
+
+const updateElements = () => {
+    updateNameCard();
+    updateEventCards();
+    updatePrefix();
+    updateContactCard();
+}
+
 window.addEventListener('resize', () => {
-    let vh = window.innerHeight * 0.01;
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
+    prevInnerHeight = window.innerHeight;
+    vh = prevInnerHeight * 0.01;
+    updateElements();
 });
+
+// Fonts affect size of elements, so we have to wait for them to load first
+document.fonts.ready.then(updateElements);
