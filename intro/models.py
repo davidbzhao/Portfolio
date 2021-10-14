f8422efc29a68f86faed242224c3be7bf22e8069
David Zhao <davidzhao058@gmail.com>
2021-10-14 01:18:34 -0400

    Make priority a pk (#18)
    Co-authored-by: David Zhao <davidbzhao@outlook.com>

--- intro/models.py
+++ intro/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 
 class Event(models.Model):
-    priority = models.IntegerField(unique=False)
+    priority = models.IntegerField(primary_key=True)
     title = models.CharField(max_length=128)
     subtitle = models.CharField(max_length=128, blank=True)
     description = models.TextField(blank=True)
