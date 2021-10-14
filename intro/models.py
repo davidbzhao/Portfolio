0e04b613d80ab559f2e6230dd088e68a0ea50f1c
David Zhao <davidzhao058@gmail.com>
2021-10-14 00:10:45 -0400

    Switch event type with priority (#13)
    Co-authored-by: David Zhao <davidbzhao@outlook.com>

--- intro/models.py
+++ intro/models.py
@@ -2,13 +2,7 @@ from django.db import models
 
 
 class Event(models.Model):
-    EVENT_TYPES = (
-        (0, 'Location'),
-        (1, 'Education'),
-        (2, 'Work'),
-        (3, 'Recreation'),
-    )
-    event_type = models.IntegerField(choices=EVENT_TYPES)
+    priority = models.IntegerField(unique=True)
     title = models.CharField(max_length=128)
     subtitle = models.CharField(max_length=128, blank=True)
     description = models.TextField(blank=True)
