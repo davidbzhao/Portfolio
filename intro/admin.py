4ddbd3d7881fa6e6c839f85966feab0d71a346d4
David Zhao <david@peregrine.io>
2023-07-15 19:32:33 -0400

    chore: Add to admin page

--- intro/admin.py
+++ intro/admin.py
@@ -1,7 +1,12 @@
 from django.contrib import admin
-from .models import Event
+from .models import Event, GridIcon
 
 class EventAdmin(admin.ModelAdmin):
     list_display = ['title', 'subtitle', 'priority', 'start_date', 'end_date']
 
+class GridIconAdmin(admin.ModelAdmin):
+    pass
+
+
 admin.site.register(Event, EventAdmin)
+admin.site.register(GridIcon, GridIconAdmin)
