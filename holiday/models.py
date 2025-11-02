0947cdafba588ff60e55b89b95c347c17022ee0a
David Zhao <david@peregrine.io>
2025-11-02 13:53:16 -0500

    Revert to GH pages (#28)
    * Create CNAME
    
    * add index.html
    
    * drop django
    
    * recreate all
    
    * add back favicon
    
    * add rubiks cube back
    
    * add back develop html
    
    * fix doggos

--- holiday/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-class Stanza(models.Model):
-    priority = models.PositiveIntegerField()
-    text = models.TextField(blank=True)
-
-class Photo(models.Model):
-    priority = models.PositiveIntegerField()
-    url = models.URLField()
